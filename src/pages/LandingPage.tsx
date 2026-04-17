76cb61fbafca9af0332fc6cd224090b63915a741
Rem-moe <abhaykv111@gmail.com>
2026-04-17 18:04:57 +0530

    feat: add GovernanceLoop component to landing page and rebrand Policy Engine as Institutional Compliance Hub

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,7 @@
 import { Header } from '../components/Header';
 import { Hero } from '../components/Hero';
 import { ProductSection } from '../components/ProductSection';
+import { GovernanceLoop } from '../components/GovernanceLoop';
 import { PipelineSection } from '../components/PipelineSection';
 import { BridgeSection } from '../components/BridgeSection';
 import { PainSection } from '../components/PainSection';
@@ -15,6 +16,7 @@ export const LandingPage = () => {
         <Hero />
         <PainSection />
         <ProductSection />
+        <GovernanceLoop />
         <BridgeSection />
         <PipelineSection />
         <DomainSection />
