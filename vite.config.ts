bc85e05f039e7a8fe82edb04c6031e2ea76128e5
Rem-moe <abhaykv111@gmail.com>
2026-05-19 10:06:47 +0530

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cloudflare()],
-  base: '/mel/',
+  base: '/',
 })
