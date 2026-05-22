e448365bfaa81f66b1ad276df29b7fec1918ef66
Rem-moe <abhaykv111@gmail.com>
2026-05-22 14:18:15 +0530

    feat: integrate PostHog for user identification and event tracking across signup and conversion flows

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.tsx'
+import { initPostHog } from './lib/posthog'
+
+initPostHog()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
